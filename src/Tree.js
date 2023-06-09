03048e672ee458dfa6c456028cc2757c84f315be
João Miguel <jrsmiguel@outlook.pt>
2023-06-09 16:41:13 +0100

    master - Update gitignore add js files

---
+++ src/Tree.js
@@ -0,0 +1,8 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.TreeSet = void 0;
+const TreeSet_1 = __importDefault(require("./TreeSet"));
+exports.TreeSet = TreeSet_1.default;
